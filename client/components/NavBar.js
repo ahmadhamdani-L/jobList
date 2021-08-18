676770c3fbe8684d88fc073308dda3803a99f4ac
unknown <ahmadhamdani-L>
2021-08-18 19:46:17 +0700

    add navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,12 +2,44 @@ import React from 'react'
 
 export default function NavBar() {
     return (
-      <div className="md:flex md:items-center md:justify-between">
-        
-      <div className="flex-1 min-w-0">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Github Jobs</h2>
-        
+      <nav class="bg-white shadow-lg">
+      <div class="max-w-6xl mx-auto px-4">
+        <div class="flex justify-between">
+          <div class="flex space-x-7">
+          
+            <div>
+              <a href="#" class="flex items-center py-4 px-2">
+                
+                <span class="font-semibold text-gray-500 text-lg"
+                  >Jobs</span>
+              </a>
+            </div>
+           
+            <div class="hidden md:flex items-center space-x-1">
+              <a
+                href=""
+                class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
+                >Home</a
+              >
+              <a
+                href=""
+                class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >Services</a
+              >
+              <a
+                href=""
+                class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >About</a
+              >
+              <a
+                href=""
+                class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                >Contact Us</a
+              >
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
+    </nav>
     )
   }
